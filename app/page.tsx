02b4b7a3f01052f3d58a7246fd149fb479687d97
Veerpaliwal <veerpaliwal901gmail.com>
2026-03-13 15:43:21 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,31 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Next.js</h1>
-      <p>My first Next.js project</p>
-    </div>
+    <>
+      <div className="slider">
+        <div className="slides">
+
+          <div className="slide">
+            <img src="https://media-ik.croma.com/prod/https://media.tatacroma.com/Croma%20Assets/CMS/LP%20Page%20Banners/2026/Campaigns/Saving%20days/Desktop/Desktop/HP%20Rotating/HP%20Rotating/HP_Rotating_savingdays_4march2026_fOzaBmWfIx.jpg?updatedAt=1772641202234" />
+          </div>
+
+          <div className="slide">
+            <img src="https://media-ik.croma.com/prod/https://media-ik.croma.com/Croma%20Assets/CMS/LP%20Page%20Banners/2026/HP%20Rotating%20Banners/March/13032026/Desktop/HP_Rotating_SW_13March2026_824F9h1NR.jpg?updatedAt=1773318684347" />
+          </div>
+
+          <div className="slide">
+            <img src="https://media-ik.croma.com/prod/https://ik.imagekit.io/bww2oimrs/Croma%20Assets/CMS/LP%20Page%20Banners/2026/HP%20Rotating%20Banners/March/11032026/HP_Rotating_Apple_MBneo_11March2026_XtdRqQHdE.jpg" />
+          </div>
+
+          <div className="slide">
+            <img src="https://media-ik.croma.com/prod/https://ik.imagekit.io/bww2oimrs/Croma%20Assets/CMS/LP%20Page%20Banners/2026/HP%20Rotating%20Banners/March/11032026/HP_Rotating_Apple_iphone17e_11March2026_moykk9lR8.jpg" />
+          </div>
+
+          <div className="slide">
+            <img src="https://media-ik.croma.com/prod/https://media-ik.croma.com/Croma%20Assets/CMS/LP%20Page%20Banners/2026/Campaigns/weekly%20circuit/06032026/HP_Rotating_Appliances_25Dec2025_QfmeLuIoe.jpg?updatedAt=1772729711550" />
+          </div>
+
+        </div>
+      </div>
+    </>
   );
 }
